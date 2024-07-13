243e3388fdd5cc6e5bf03e901e7ec5c4e6482994
degencodebeast <degencodebeast@protonmail.com>
2024-07-13 03:15:37 +0100

    feat: added create_proposal instruction

--- programs/dao-voting-program/src/instructions/mod.rs
+++ programs/dao-voting-program/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod initialize_dao;
 pub mod vote;
 pub mod issue_token;
 pub mod init_stake;
+pub mod create_proposal;
 
 pub use initialize_dao::*;
 pub use vote::*;
 pub use issue_token::*;
 pub use init_stake::*;
+pub use create_proposal::*;
